2a333dbf4f9014b9b0721a161b5dc656a2aa2730
Set <6942774+setzerbomb@users.noreply.github.com>
2019-10-06 03:34:02 -0300

    Finalização do front em React

--- bakcend/src/controllers/SpotController.js
+++ bakcend/src/controllers/SpotController.js
@@ -1,63 +1,53 @@
-const Spot = require('../models/Spot');
-const User = require('../models/User');
-const fs = require('fs');
-const {
-    promisify
-} = require('util')
+const Spot = require("../models/Spot");
+const User = require("../models/User");
+const fs = require("fs");
+const { promisify } = require("util");
 
 module.exports = {
-    async index(req, res) {
-        const {
-            tech
-        } = req.query;
+  async index(req, res) {
+    const { tech } = req.query;
 
-        const spots = await Spot.find({
-            techs: tech
-        });
+    const spots = await Spot.find({
+      techs: tech
+    });
 
-        return res.json(spots);
-    },
-    async store(req, res) {
-        const {
-            filename
-        } = req.file;
-        const {
-            company,
-            techs,
-            price
-        } = req.body;
-        const {
-            user_id
-        } = req.headers;
+    return res.json(spots);
+  },
+  async store(req, res) {
+    const { filename } = req.file;
+    const { company, techs, price } = req.body;
+    const { user_id } = req.headers;
 
-        const user = await User.findById(user_id);
+    const user = await User.findById(user_id);
 
-        if (!user) {
-            return res.status(400).json({
-                error: 'Usuário não existe'
-            })
-        }
-
-        if ((await Spot.countDocuments({
-                company
-            })) < 1) {
-            const spot = await Spot.create({
-                user: user_id,
-                thumbnail: filename,
-                company,
-                techs: techs.split(',').map(tech => tech.trim()),
-                price
-            })
-
-            return res.json(spot);
-        }
-
-        const unlinkAsync = promisify(fs.unlink);
-
-        await unlinkAsync(req.file.path);
-
-        return res.status(400).json({
-            error: 'Empresa já cadastrada'
-        })
+    if (!user) {
+      return res.status(400).json({
+        error: "Usuário não existe"
+      });
     }
+
+    if (
+      (await Spot.countDocuments({
+        company
+      })) < 1
+    ) {
+      const spot = await Spot.create({
+        user: user_id,
+        thumbnail: filename,
+        company,
+        techs: techs.split(",").map(tech => tech.trim()),
+        price
+      });
+
+      return res.json(spot);
+    }
+
+    const unlinkAsync = promisify(fs.unlink);
+
+    await unlinkAsync(req.file.path);
+
+    return res.status(400).json({
+      error: "Empresa já cadastrada"
+    });
+  }
 };
